41ba8f711c2341a26a252ec999c63b8ca49b1222
MahdiSadat <59486542+mahdisaadaat@users.noreply.github.com>
2022-12-24 20:24:08 +0330

    Update app.js

--- AllProject/lg-online-video-player/app.js
+++ AllProject/lg-online-video-player/app.js
@@ -1,5 +1,5 @@
 const links = [
   "http://cloud2.nightblackmovie.online/dl17/jack/Movie/2021/The.Bargain/The.Bargain.2021.1080p.WEB-DL.x264.AAC-YIFY.NightMovie.mp4?key=416228_m5lQ26g98z6U1PmSt9RUdglmt1671897448",
-  "adads",
+  "http://cloud2.nightblackmovie.online/dl15/arashM2/Movie.Sub/0/Day99/Avatar.2009/Avatar.2009.Collectors.Extended.Cut.1080p.BluRay.6CH.x264.SoftSub.NightMovie.mkv?key=416228_HFLM1kAR4BizH74UfRGf6Almt1671900693",
 ];
 document.getElementById("linksList").innerHTML = links;
