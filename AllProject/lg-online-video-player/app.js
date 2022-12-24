9add13341130dfae5726e61c639c3603ce7dff11
Mahdisaadaat <mahdisaadaat@gmail.com>
2022-12-24 20:46:57 +0330

    Update app.js

--- AllProject/lg-online-video-player/app.js
+++
@@ -1,5 +0,0 @@
-const links = [
-  "http://cloud2.nightblackmovie.online/dl17/jack/Movie/2021/The.Bargain/The.Bargain.2021.1080p.WEB-DL.x264.AAC-YIFY.NightMovie.mp4?key=416228_m5lQ26g98z6U1PmSt9RUdglmt1671897448",
-  "adads",
-];
-document.getElementById("linksList").innerHTML = links;
