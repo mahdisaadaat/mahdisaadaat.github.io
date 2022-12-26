4010b6c3075b0d9511be89bb9ff6d8a218b4e541
Mahdisaadaat <mahdisaadaat@gmail.com>
2022-12-26 18:41:57 +0330

    Create AddLinks.js

---
+++ AllProject/lgNVP/components/AddLinks.js
@@ -0,0 +1,9 @@
+export const Addlinks = () => {
+  return `
+    <div class="addLinks input-group rounded">
+        <label class="addLinkLabel m-2">Add Links</label>
+        <input class="addLinkInput m-2" type="text" />
+        <button class="m-2">Add</button>
+    </div>
+    `;
+};
