7a228656a29ebc24fe381b1db480524ec041c1de
MahdiSadat <59486542+mahdisaadaat@users.noreply.github.com>
2023-02-01 14:00:46 +0330

    Update MoviesAndSeries.js

--- AllProject/lg-online-video-player/MoviesAndSeries.js
+++ AllProject/lg-online-video-player/MoviesAndSeries.js
@@ -1,7 +1,7 @@
 const urls = [
   {
     id: 1,
-    movieName: "Avatar.2009",
+    movieName: "پوست شیر",
     url: "http://tb28.trainbit.com:8080/files/9708642484/v363494752654F6E3455456A683550355331564757582F446C62424B4A672F57714565484E42676E6C495434304E7045322B466B766E773D3D/poosteshir-S02E07-720.mp4",
     img: "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_.jpg",
     movieSubtitle: "GhostraiderSub.vtt",
