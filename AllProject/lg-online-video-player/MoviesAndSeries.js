d3ff0ca39e0cfd770995d9c298dc4c2665624c17
MahdiSadat <59486542+mahdisaadaat@users.noreply.github.com>
2023-03-13 15:15:46 +0330

    Update MoviesAndSeries.js

--- AllProject/lg-online-video-player/MoviesAndSeries.js
+++ AllProject/lg-online-video-player/MoviesAndSeries.js
@@ -1,8 +1,8 @@
 const urls = [
   {
     id: 1,
-    movieName: "اکتور 7",
-    url: "https://ir91.uploadboy.com/d/a18qfdo5xrqm/snnlms6gjdfx3pohwiuw7fuc5csc4jwhxbbhiaeh322iyozyjw27fqm3nin4nhbyxgkne7ym/Actor-E07.720.mp4",
+    movieName: "ashiqui 2",
+    url: "https://cdn.irdanlod.ir/?s=4&f=/files/Movie/2013/A/Aashiqui.2.2013.1080p.Farsi.Subbed.mkv",
     img: "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_.jpg",
     movieSubtitle: "GhostraiderSub.vtt",
   },
