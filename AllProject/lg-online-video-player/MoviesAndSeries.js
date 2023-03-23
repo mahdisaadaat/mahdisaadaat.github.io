3f93f43297485e745fcb43a34222845e62aba86c
MahdiSadat <59486542+mahdisaadaat@users.noreply.github.com>
2023-03-23 14:36:07 +0330

    Update MoviesAndSeries.js

--- AllProject/lg-online-video-player/MoviesAndSeries.js
+++ AllProject/lg-online-video-player/MoviesAndSeries.js
@@ -5,6 +5,12 @@ const urls = [
     url: "https://ir71.uploadboy.com/d/m2cy7t9833w2/sfnltmwfjdfx3pohw4uwfr6w7lktgh7dsvazu4z5re5ufurvpasz4sm64c73jb7ymrvnihve/Baradaran-e-Leila-720.mp4",
     img: "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_.jpg",
     movieSubtitle: "",
+  },{
+    id: 2,
+    movieName: "actor 9",
+    url: "https://ir31.uploadboy.com/d/drbfv5nqyp9o/rnnnl4gfjdfx3pohjixh5smcwtntez7lci275wmseca2va4c7pglcp42v3bgzbgkrobaufk7/Actor-E09.1080.mp4",
+    img: "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_.jpg",
+    movieSubtitle: "",
   },
 ];
 
