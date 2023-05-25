10ea4955f92dbedb0d9ce5db5bd34f5f04fbff91
MahdiSadat <59486542+mahdisaadaat@users.noreply.github.com>
2023-05-25 14:14:36 +0330

    Delete App.js

--- AllProject/lgNVP/App.js
+++
@@ -1,6 +0,0 @@
-import { Nav } from "./components/Nav.js";
-import { Player } from "./components/Player.js";
-import { Addlinks } from "./components/AddLinks.js";
-export const App = () => {
-  return [Nav("Mahdi"), Addlinks(), Player()];
-};
